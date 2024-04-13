f20dc121199c5a0ff1df3692cdaa5024f87d274b
Jhostyn <Jhostynchacon2@gmail.com>
2024-04-13 10:33:13 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto en clase/Program.cs
@@ -0,0 +1,25 @@
+﻿using Clase_Erencia.Clase_Base;
+using Clase_Erencia.Clase_Hija;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Clase_Erencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Xbox xb = new Xbox();
+            xb.GamePass = true;
+            xb.AnioLanzamiento = 2022;
+            xb.Marca = "Xbox Series X";
+            string resultado2 = xb.MostrarDetallesXbox();
+            Console.WriteLine(resultado2);
+        }
+    }
+}
